1f1d9c98773b4226526d552fa657e2542a712715
GMihai00 <mihai.gherghinscu15@gmail.com>
2024-10-26 16:28:41 +0300

    tweaks

--- mihaigh/models/BinDetectClient.py
+++ mihaigh/models/BinDetectClient.py
@@ -28,7 +28,7 @@ class YOLOClient:
         for r in results:
             for box in r.boxes:
                 confidence = float(box.conf[0].cpu().numpy())
-                if confidence >= 0.71:
+                if confidence >= 0.69:
                     # print(f"CONFIDENCE SCORE: {confidence}")
                     detections.append([confidence])
         
