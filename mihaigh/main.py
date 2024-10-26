068ede347039962ddbb5446f885cafdd6eb58e09
GMihai00 <mihai.gherghinscu15@gmail.com>
2024-10-26 09:53:09 +0300

    something

--- mihaigh/main.py
+++ mihaigh/main.py
@@ -1,23 +1,37 @@
 from models.ObjectTracker import ObjectTracker
 import time
+import threading
 
 def main():
-
-    # binTracker = ObjectTracker("/home/mgherghinescu/projects/HackTeck2024/Dataset/Videori 240520/240520/240520_055427_055527.mp4")
     
+    # 15 min long in 46 sec
     binTracker = ObjectTracker("/home/mgherghinescu/projects/HackTeck2024/Dataset/output_merged_video.mp4")
+    
+    # 3h video just for looking for issues
+    # binTracker = ObjectTracker("/home/mgherghinescu/projects/HackTeck2024/Dataset/mergedday1_3h.mp4")
     
     binTracker.start_tracking(True);
     
     print("Program is running. Press Ctrl+C to stop.")
+    start_time = time.time() 
+    stop_event = threading.Event()  # Event to signal stop
+    
+    # try:
+    #     while True:
+    #         time.sleep(1)  # Keeps the program alive and responsive
+    # except KeyboardInterrupt:
+    #     print("\nProgram stopped by user.")
+        
     try:
-        while True:
-            time.sleep(1)  # Keeps the program alive and responsive
+        stop_event.wait()  # Wait indefinitely until the event is set
     except KeyboardInterrupt:
         print("\nProgram stopped by user.")
         
     binTracker.stop_tracking();
     
+    end_time = time.time() 
+    elapsed_time = end_time - start_time
+    print(f"\nProgram stopped by user. Time elapsed: {elapsed_time:.2f} seconds")
     print("EXITING MAIN")
     
 
